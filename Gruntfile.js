05df5dc738918a38fb3d19782f1c28f5fcae1890
Denis Stoyanov <stoyanov.gr@email.com>
2014-05-13 02:04:51 +0300

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                input: "C:\Users\gromm\Desktop\rx-book",
+                github: "xgrommx/rx-book"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '_book'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};

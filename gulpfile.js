818743f431843994736877ce477205a3952e1ed5
xgrommx <stoyanov.gr@gmail.com>
2015-11-26 06:52:18 +0200

    Update 2015-11-26T04:52:07.754Z

--- gulpfile.js
+++ gulpfile.js
@@ -50,9 +50,12 @@ gulp.task('default', ['css', 'js', 'assets'], function() {
 gulp.task('build', shell.task(['gitbook build']));
 gulp.task('serve', shell.task(['gitbook serve']));
 gulp.task('pdf', shell.task(['gitbook pdf']));
+gulp.task('update', shell.task(['git add . && git commit -m "Book has been updated" && git push origin master']));
 
 gulp.task('deploy', ['build'], function() {
   return gulp.src('./_book/**/*')
     .pipe(ghPages());
 });
+
+gulp.task('deploy:update', ['deploy', 'update']);
 
